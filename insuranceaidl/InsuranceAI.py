57fe8c1cd4e142ebfe00d5b4b34fe9d6890efc92
Bernard Salanie <bs2237@columbia.edu>
2022-11-15 17:28:08 -0500

    added callouts

--- insuranceaidl/InsuranceAI.py
+++ insuranceaidl/InsuranceAI.py
@@ -3,8 +3,8 @@
 # %% auto 0
 __all__ = ['data88', 'y1', 'y2', 'w', 'X']
 
-# %% ../nbs/index.ipynb 4
+# %% ../nbs/index.ipynb 6
 data88 = read_data("j88")
 
-# %% ../nbs/index.ipynb 6
+# %% ../nbs/index.ipynb 8
 y1, y2, w, X = select_variables(data88)
